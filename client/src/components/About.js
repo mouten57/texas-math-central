ac8d7ce30844f1a5fd8171b7a079d7900cca3e5c
matt.outen <matt.outen@mattouten.local>
2020-08-28 09:14:03 -0400

    updated about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import {
   Container,
   Divider,
-  List,
+  Segment,
   Placeholder,
-  Breadcrumb
-} from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+  Breadcrumb,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -18,40 +18,51 @@ const About = () => {
         <Breadcrumb.Divider />
         <Breadcrumb.Section active>About</Breadcrumb.Section>
       </Breadcrumb>
-      <Container text textAlign="justified" style={{ marginTop: '40px' }}>
+      <Container text textAlign="justified" style={{ marginTop: "40px" }}>
         <h2>About</h2>
         <Divider />
         <p>
-          This website is all about teachers. It was designed and created with
-          one goal in mind: to get all the best math resources in one location.
-          No more aimless searching.
+          TMC was created with one goal in mind: to get all the best math
+          resources in one location. This site is specific to Texas 5th Grade
+          math teachers, with plans to expand in the near future. If you are a
+          5th grade math teacher, you're in the right place.
         </p>
-        <p>Find the most popular (free) resources today.</p>
+        <p>
+          My name is Matt Outen and I have taught 5th grade for the last 6
+          years. If you need help with anything on the site, or have
+          suggestions, please feel free to{" "}
+          <a href="mailto:texasmathcentral@gmail.com" target="blank">
+            contact me
+          </a>
+          .
+        </p>
 
-        <h3 style={{ marginTop: '50px' }}>Why Texas Math Central?</h3>
+        <h3 style={{ marginTop: "50px" }}>Find Resources</h3>
         <Divider />
-        <List
-          items={[
-            'Resources voted on by Texas Math Teachers',
-            'Everything is FREE!',
-            '...fill this in later...'
-          ]}
-        />
-
-        <h4 style={{ marginTop: '50px' }}>Other Info:</h4>
-        <Divider />
-        <Placeholder fluid>
-          <Placeholder.Header image>
-            <Placeholder.Line />
-            <Placeholder.Line />
-          </Placeholder.Header>
-          <Placeholder.Paragraph>
-            <Placeholder.Line />
-            <Placeholder.Line />
-            <Placeholder.Line />
-            <Placeholder.Line />
-          </Placeholder.Paragraph>
-        </Placeholder>
+        <Segment raised size="large" padded="very">
+          <h2 style={{ textAlign: "center" }}>Find Resources</h2>
+          Finding resources is as easy as heading over to our{" "}
+          <a href="/units">Units</a> page and finding whatever unit you are
+          currently in. This covers most of the 5th grade math resources. They
+          aren't listed by TEKS; they are organized into broader categories that
+          made more sense to me as a teacher.
+        </Segment>
+        <Segment raised size="large" padded="very">
+          <h2 style={{ textAlign: "center" }}>Add Your Own Resources</h2>
+          Have something awesome that you want to share? Go to our{" "}
+          <a href="resources/new">add resource form</a> and share away! We have
+          the capabilities of adding pictures, docs, pdfs, or just plain old
+          links!
+        </Segment>
+        <Segment raised size="large" padded="very">
+          <h2 style={{ textAlign: "center" }}>
+            Collaborate With Other Teachers
+          </h2>
+          What's better than knowing the best way to use a resource? Add
+          comments, reply to others, or leave suggestions using our comment
+          form! Here's an example:{" "}
+          <a href="units/boy/5c11b93b63265f23141bbabf">Sample Resource</a>
+        </Segment>
       </Container>
     </div>
   );
