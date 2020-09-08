ff3562f94028061ef886279cf8019bb93121b296
Matt Outen <mattoutenphysed@gmail.com>
2020-09-07 20:22:34 -0400

    re-work conversion. use convertapi for all

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -7,7 +7,7 @@ const keys = require("../config/keys/keys");
 const deleteUploads = require("../middlewares/deleteUploads");
 var { s3 } = awsConfig;
 var convertapi = require("convertapi")(keys.convertapi_KEY);
-var im = require("imagemagick");
+// var im = require("imagemagick");
 const path = require("path");
 const { settings } = require("cluster");
 const filetype_settings = require("../helpers/filetype_settings");
@@ -79,44 +79,43 @@ module.exports = {
             let file_ext = path.extname(files[i].filename).substring(1);
             let file_path = `./uploads/${files[i].filename}`;
 
-            //only do first conversion if NOT a pdf
-            if (file_ext != "pdf") {
-              let result = await convertapi.convert(
-                "pdf",
-                filetype_settings(file_path, file_ext),
-                file_ext
-              );
-              await result.file.save(`./uploads/${files[i].filename}.pdf`);
-            }
-            function convertImage() {
-              return new Promise((resolve, reject) => {
-                im.convert(
-                  [
-                    "-flatten",
-                    "-background",
-                    "#fff",
-                    "-quality",
-                    100,
-                    // "-resize",
-                    // "1530x1980",
-                    `./uploads/${files[i].filename}${
-                      file_ext == "pdf" ? "" : ".pdf"
-                    }`,
-                    `./uploads/${files[i].filename}.jpg`,
-                  ],
-                  function (err, stdout) {
-                    if (err) {
-                      reject(err);
-                    }
-                    resolve(stdout);
-                  }
+            async function convertImage() {
+              try {
+                var file_to_thumbnail = file_path;
+
+                if (file_ext != "pdf") {
+                  let result = await convertapi.convert(
+                    "pdf",
+                    filetype_settings(file_path, file_ext),
+                    file_ext
+                  );
+                  //over-write original file declaration
+                  var file_to_thumbnail = await result.file.save(
+                    `./uploads/${files[i].filename}.pdf`
+                  );
+                }
+
+                let result = await convertapi.convert(
+                  "thumbnail",
+                  {
+                    File: file_to_thumbnail,
+                    PageRange: "1",
+                    ImageResolution: "72",
+                  },
+                  "pdf"
                 );
-              });
+                let image_file = await result.file.save(
+                  `./uploads/${files[i].filename}.jpg`
+                );
+              } catch (err) {
+                throw err;
+              }
             }
 
-            //optional: use imagemagick to convert pdf to jpg
+            // await result of async operation
             await convertImage();
 
+            //save to s3
             let s3Data = await s3
               .upload({
                 Bucket: "texas-math-central",
@@ -125,33 +124,9 @@ module.exports = {
               })
               .promise();
             files[i].previewLink = s3Data.Location;
-
-            //optional: use convertapi again to convert pdf to thumbnail
-            // await convertapi
-            //   .convert(
-            //     "thumbnail",
-            //     {
-            //       File: `./uploads/${files[i].filename}${
-            //         !file_ext.includes("pdf") ? ".pdf" : ""
-            //       }`,
-            //       PageRange: "1",
-            //       ImageResolution: "72",
-            //     },
-            //     "pdf"
-            //   )
-            //   .then(async function (result) {
-            //     await result.saveFiles(`./uploads/${files[i].filename}.jpg`);
-            //     let s3Data = await s3
-            //       .upload({
-            //         Bucket: "texas-math-central",
-            //         Key: `${files[i].filename}.jpg`,
-            //         Body: fs.readFileSync(`./uploads/${files[i].filename}.jpg`),
-            //       })
-            //       .promise();
-            //     files[i].previewLink = s3Data.Location;
-            //   });
           }
 
+          //save original file to s3
           let s3Data = await s3.upload(params).promise();
           files[i].s3Object = s3Data;
           files[i].s3Link = s3Data.Location;
