3086f13320c2b516b85be09eac62bd9f4b8138a1
matt.outen <matt.outen@mattouten.local>
2020-09-11 20:52:57 -0400

    new and improved filepreview is ready

--- middlewares/filepreview/test.js
+++ middlewares/filepreview/test.js
@@ -1,58 +1,64 @@
-const filepreview = require("./filepreview.js");
+const { degrees, PDFDocument, rgb, StandardFonts } = require("pdf-lib");
 const fs = require("fs");
-const path = require("path");
-const unoconv = require("awesome-unoconv");
-var im = require("imagemagick");
 
-const sourceFilePath = path.resolve("./documents/example.xlsx");
-const outputFilePath = path.resolve("./documents/example.pdf");
+async function run_test() {
+  // This should be a Uint8Array or ArrayBuffer
+  // This data can be obtained in a number of different ways
+  // If your running in a Node environment, you could use fs.readFile()
+  // In the browser, you could make a fetch() call and use res.arrayBuffer()
+  const existingPdfBytes = fs.readFileSync("./test.pdf");
 
-unoconv
-  .convert(sourceFilePath, outputFilePath)
-  .then((result) => {
-    im.convert(
-      [
-        "-flatten",
-        "-background",
-        "#fff",
-        "-quality",
-        100,
-        "-resize",
-        "500x800",
-        "/Users/matt.outen/Desktop/projects/texas-math-central/middlewares/filepreview/documents/example.pdf",
-        "/Users/matt.outen/Desktop/projects/texas-math-central/middlewares/filepreview/documents/example-widthxheight.jpg",
-      ],
-      function (err, stdout) {
-        if (err) throw err;
-        console.log("stdout:", stdout);
-      }
-    );
-    console.log(result); // return outputFilePath
-  })
-  .catch((err) => {
-    console.log(err);
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
+  // Embed the Helvetica font
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  // Get the width and height of the first page
+  const { width, height } = firstPage.getSize();
+
+  // Draw a string of text diagonally across the first page
+  for (i in pages) {
+    pages[i].drawText("This is just a preview!", {
+      x: 75,
+      y: height / 2 + 300,
+      size: 60,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-35),
+    });
+    pages[i].drawText("This is just a preview!", {
+      x: 45,
+      y: height / 2 + 150,
+      size: 60,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-35),
+    });
+    pages[i].drawText("This is just a preview!", {
+      x: 15,
+      y: height / 2,
+      size: 60,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-35),
+    });
+  }
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save();
+
+  fs.writeFile("test_preview.pdf", pdfBytes, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
   });
+}
+run_test();
 
-// const fileName = "example.xlsx";
-// const filePath = path.resolve("documents", fileName);
-// const options = {
-//   width: 800,
-//   height: 1100,
-//   quality: 100,
-//   background: "#fff",
-//   pdf: true,
-//   keepAspect: true,
-//   pagerange: "1",
-//   pdf_path: path.resolve("documents", "pdfs"),
-// };
-// const outPath = path.resolve(
-//   "documents",
-//   `${fileName.replace(/\.[^/.]+$/, "")}-widthxheight.jpg`
-// );
-// filepreview
-//   .generateAsync(filePath, outPath, options)
-//   .then((response) => {
-//     //console.log(response);
-//     console.log(response);
-//   })
-//   .catch((error) => console.log(error));
+// For example, `pdfBytes` can be:
+//   • Written to a file in Node
+//   • Downloaded from the browser
+//   • Rendered in an <iframe>
