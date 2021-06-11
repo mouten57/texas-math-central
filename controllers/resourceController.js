da45f0b062cbd61f296d94dd646bd23eef5bfa2f
Matt Outen <mattoutenphysed@gmail.com>
2021-06-11 16:22:59 -0400

    'log'

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -210,7 +210,7 @@ module.exports = {
                   }
                 );
               } catch (err) {
-                res.send(err);
+                console.log(err);
               }
             }
 
